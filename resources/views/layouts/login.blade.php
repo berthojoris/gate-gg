13d07166bb61d8b8994253c56207cfcfe51e2e5d
Albertho Malaquena Joris <ajoris@gudanggaramtbk.com>
2020-02-05 14:04:07 +0700

    Remove unused tag

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -5,8 +5,6 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0,minimal-ui">
         <title>Login</title>
-        <meta content="Admin Dashboard" name="description">
-        <meta content="Themesbrand" name="author">
         <link rel="shortcut icon" href="{{ asset('template/images/favicon.ico') }}">
         <link href="{{ asset('template/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('template/css/metismenu.min.css') }}" rel="stylesheet" type="text/css">
