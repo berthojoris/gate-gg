a12a15fa10082156c5ca492678f7062c920cd553
Albertho Malaquena Joris <ajoris@gudanggaramtbk.com>
2020-02-06 18:38:36 +0700

    Authentication using passport

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,15 @@
 namespace App\Exceptions;
 
 use Exception;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Laravel\Passport\Exceptions\OAuthServerException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +58,64 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->ajax() || $request->isJson()) {
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json([
+                    'error' => 'Entry for '.str_replace('App\\', '', $exception->getModel()).' not found',
+                    'message' => $exception->getMessage(),
+                    'code' => 404
+                ], 404);
+            } else if ($exception instanceof AuthorizationException) {
+                return response()->json([
+                    'error' => 'Authorization Exception',
+                    'message' => $exception->getMessage(),
+                    'code' => 401
+                ], 401);
+            } else if ($exception instanceof AuthenticationException) {
+                return response()->json([
+                    'error' => 'Authentication Exception',
+                    'message' => $exception->getMessage(),
+                    'code' => 403
+                ], 403);
+            } else if ($exception instanceof RequestException) {
+                return response()->json([
+                    'error' => 'Request Exception',
+                    'message' => $exception->getMessage(),
+                    'code' => 500
+                ], 500);
+            } else if ($exception instanceof MethodNotAllowedHttpException) {
+                return response()->json([
+                    'error' => 'Method Not Allowed Http Exception',
+                    'message' => $exception->getMessage(),
+                    'code' => 405
+                ], 405);
+            } else if ($exception instanceof TokenMismatchException) {
+                return response()->json([
+                    'error' => 'Token Mismatch Exception',
+                    'message' => $exception->getMessage(),
+                    'code' => 498
+                ], 498);
+            } else if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'error' => 'Not Found Http Exception',
+                    'message' => $exception->getMessage(),
+                    'code' => 410
+                ], 410);
+            } else if ($exception instanceof OAuthServerException) {
+                return response()->json([
+                    'error' => 'OAuth Server Exception',
+                    'message' => $exception->getMessage(),
+                    'code' => 403
+                ], 403);
+            }
+            else {
+                return response()->json([
+                    'error' => 'Not Define Exception',
+                    'message' => 'Not Define Exception',
+                    'code' => 404
+                ], 404);
+            }
+        }
         return parent::render($request, $exception);
     }
 }
--- resources/views/shared/menu_header.blade.php
+++ resources/views/shared/menu_header.blade.php
@@ -69,7 +69,12 @@
                     <a class="dropdown-toggle nav-link arrow-none waves-effect nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false"><img src="{{ asset('template/images/users/user-4.jpg') }}" alt="user" class="rounded-circle"></a>
                     <div class="dropdown-menu dropdown-menu-right profile-dropdown">
                         <!-- item--><a class="dropdown-item" href="#"><i class="mdi mdi-account-circle m-r-5"></i> Profile</a> <a class="dropdown-item" href="#"><i class="mdi mdi-wallet m-r-5"></i> My Wallet</a> <a class="dropdown-item d-block" href="#"><span class="badge badge-success float-right">11</span><i class="mdi mdi-settings m-r-5"></i> Settings</a> <a class="dropdown-item" href="#"><i class="mdi mdi-lock-open-outline m-r-5"></i> Lock screen</a>
-                        <div class="dropdown-divider"></div><a class="dropdown-item text-danger" href="#"><i class="mdi mdi-power text-danger"></i> Logout</a></div>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item text-danger" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="mdi mdi-power text-danger"></i> Logout</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
                 </div>
             </li>
         </ul>
