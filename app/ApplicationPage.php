5027602c7d200f4a7b314d1993462f1e09378848
Bertho <berthojoris@gmail.com>
2020-12-04 16:38:26 +0700

    ApplicationPage model relation

--- app/ApplicationPage.php
+++ app/ApplicationPage.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ApplicationPage extends Model
 {
-    //
+    protected $connection = 'local_gate';
+    protected $table = 'ggid_applicationpage';
+
+    public function user()
+    {
+        return $this->belongsTo('App\Myuser', 'author_id', 'id');
+    }
+
+    public function application()
+    {
+        return $this->belongsTo('App\Application', 'application_id', 'id');
+    }
 }
