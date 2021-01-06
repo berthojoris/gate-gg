1100e1e8d137ae36eddae019a6690fd098e51d6a
Bertho <berthojoris@gmail.com>
2021-01-06 18:19:41 +0700

    use rightJoin

--- app/Point.php
+++ app/Point.php
@@ -25,7 +25,7 @@ class Point extends Model
     {
         $model = DB::connection('online_gate')->table('ggid_point')
             ->select(DB::raw('user_id, SUM(IFNULL(amount,0)) as total_point, ggid_myuser.name, ggid_myuser.email'))
-            ->leftJoin('ggid_myuser', 'ggid_myuser.id', '=', 'ggid_point.user_id')
+            ->rightJoin('ggid_myuser', 'ggid_myuser.id', '=', 'ggid_point.user_id')
             ->groupBy('user_id');
         return DataTables::of($model)->toJson();
     }
